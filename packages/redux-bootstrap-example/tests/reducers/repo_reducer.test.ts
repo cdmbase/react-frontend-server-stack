ba62ccf7049b7bffaf78283e390370c1dc08e40a
Bogdan Surai <bs08@ukr.net>
2017-04-09 13:30:14 +0300

    repo_reduser test works well

--- packages/redux-bootstrap-example/tests/reducers/repo_reducer.test.ts
+++ packages/redux-bootstrap-example/tests/reducers/repo_reducer.test.ts
@@ -2,20 +2,21 @@ import reposReducer from "../../src/reducers/repos_reducer";
 import ACTION_TYPES from "../../src/constants/action_types";
 import { expect } from "chai";
 import * as mocha from "mocha";
+import consoleLog from "../consoleLog";
 
 describe("Repo Reducer", () => {
 
     it("Should set the initial state", () => {
         let defaultSate = reposReducer(undefined, { type: "@@INIT" });
-        expect(defaultSate.get("loading")).eql(false);
-        expect(defaultSate.get("reposCount")).eql(0);
+        expect(defaultSate["loading"]).eql(false);
+        expect(defaultSate["reposCount"]).eql(0);
     });
 
     it("Should be able to indicate that repos are loading", () => {
         let defaultSate = reposReducer(undefined, { type: "@@INIT" });
         let state = reposReducer(defaultSate, { type: ACTION_TYPES.ADD_REPO_BEGIN });
-        expect(state.get("loading")).eql(true);
-        expect(state.get("reposCount")).eql(0);
+        expect(state["loading"]).eql(true);
+        expect(state["reposCount"]).eql(0);
     });
 
     it("Should be able to indicate that repos are loading", () => {
@@ -23,13 +24,11 @@ describe("Repo Reducer", () => {
         let defaultSate = reposReducer(undefined, { type: "@@INIT" });
 
         let state1 = reposReducer(defaultSate, { type: ACTION_TYPES.ADD_REPO_SUCCESS,  });
-        expect(state1.get("loading")).eql(false);
-        expect(state1.get("reposCount")).eql(1);
+        expect(state1["loading"]).eql(false);
+        expect(state1["reposCount"]).eql(1);
 
         let state2 = reposReducer(state1, { type: ACTION_TYPES.ADD_REPO_SUCCESS,  });
-        expect(state2.get("loading")).eql(false);
-        expect(state2.get("reposCount")).eql(2);
-
+        expect(state2["loading"]).eql(false);
+        expect(state2["reposCount"]).eql(2);
     });
-
 });
